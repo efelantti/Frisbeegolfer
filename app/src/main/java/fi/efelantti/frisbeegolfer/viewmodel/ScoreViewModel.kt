58e39965bf2381d8b8c749c87ab5907cfd50ae86
Antti Karvinen <anttikarvinen@hotmail.com>
2021-03-20 13:02:53 +0200

    - Continued work on ScoreViewModel.kt.

--- app/src/main/java/fi/efelantti/frisbeegolfer/viewmodel/ScoreViewModel.kt
+++ app/src/main/java/fi/efelantti/frisbeegolfer/viewmodel/ScoreViewModel.kt
@@ -20,8 +20,11 @@ class ScoreViewModel(application: Application, private val roundId: OffsetDateTi
 
     private val repository: Repository
     private val mCurrentRound: LiveData<RoundWithScores>
-    var currentScoreIndex: Int = -1
+    private var currentScoreIndex: Int = -1
     val currentRound: RefreshableLiveData<RoundWithScores>
+    private val sortedScores: LiveData<List<ScoreWithPlayerAndHole>>
+    val currentScore: LiveData<ScoreWithPlayerAndHole>
+
 
     init {
         val database = FrisbeegolferRoomDatabase.getDatabase(
@@ -33,8 +36,18 @@ class ScoreViewModel(application: Application, private val roundId: OffsetDateTi
             repository.getRoundWithRoundId(roundId)
         }
         currentRound = mCurrentRound
+        sortedScores = Transformations.map(mCurrentRound,
+            {
+                sortRound(it.scores)
+            })
+        currentScore = Transformations.map(sortedScores, {
+            it.get(currentScoreIndex)
+        })
     }
 
+    /*
+    Used to force refresh the observers of the LiveData object.
+     */
     fun refresh() {
         currentRound.refresh()
     }
@@ -43,16 +56,22 @@ class ScoreViewModel(application: Application, private val roundId: OffsetDateTi
         repository.update(score)
     }
 
-    fun sortRound(scores: List<ScoreWithPlayerAndHole>): List<ScoreWithPlayerAndHole> {
+    /*
+    Sort the scores list by (hole number, player id). TODO - Avoid this by changing @RELATION to two queries?
+    (Refer to https://stackoverflow.com/questions/48315261/using-rooms-relation-with-order-by/64321494)
+     */
+    private fun sortRound(scores: List<ScoreWithPlayerAndHole>): List<ScoreWithPlayerAndHole> {
         return scores.sortedWith(compareBy({ it.hole.holeNumber }, { it.player.id }))
         }
 
-    fun getCurrentScore(): ScoreWithPlayerAndHole? {
-        return currentRound.value?.scores?.get(currentScoreIndex)
-    }
-
-    fun initCurrentScoreIndex(scores: List<ScoreWithPlayerAndHole>) {
+    /*
+    Used to initialize current score, if it's not already initialized.
+    Sets the index to either the index of the first hole that's not yet scored OR to the index of the last hole, if all are already scored.
+     */
+    fun initCurrentScoreIndex() {
         if(currentScoreIndex == -1) {
+            val scores = sortedScores.value
+            if (scores == null) throw IllegalArgumentException("Cannot init current score index - sorted scores was null.")
             val firstNotScoredHole = scores.filter { it.score.result == 0 }.firstOrNull()
             if (firstNotScoredHole == null) currentScoreIndex = scores.count() - 1
             else currentScoreIndex = scores.indexOf(firstNotScoredHole)
