973e6b7af9d709fa37ff8a74b4cd33754cb2e586
Antti Karvinen <anttikarvinen@hotmail.com>
2021-03-26 18:49:47 +0200

    - Started work on aggregating hole statistics.

--- app/src/main/java/fi/efelantti/frisbeegolfer/dao/RoundDao.kt
+++ app/src/main/java/fi/efelantti/frisbeegolfer/dao/RoundDao.kt
@@ -2,6 +2,7 @@ package fi.efelantti.frisbeegolfer.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import fi.efelantti.frisbeegolfer.model.HoleStatistics
 import fi.efelantti.frisbeegolfer.model.Round
 import fi.efelantti.frisbeegolfer.model.RoundWithCourseAndScores
 import fi.efelantti.frisbeegolfer.model.Score
@@ -32,4 +33,11 @@ interface RoundDao {
     @Transaction
     @Query("SELECT * FROM Round WHERE dateStarted =:roundId")
     fun getRoundWithId(roundId: OffsetDateTime): LiveData<RoundWithCourseAndScores>
+
+    /*
+    Gets the best (minimum) result, average, standard deviation and latest result from the score table.
+     */
+    @Transaction
+    @Query("SELECT MIN(result) AS bestResult, AVG(result) AS avgResult, STDEVP(result) AS sdResult FROM Score WHERE playerId=:playerId AND holeId=:holeId UNION SELECT result AS latestResult FROM Score WHERE playerId=:playerId AND holeId=:holeId ORDER BY id desc")
+    fun getHoleStatistics(playerId: Long, holeId: Long): LiveData<HoleStatistics>
 }
