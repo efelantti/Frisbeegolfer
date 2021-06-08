7322b9916c45211f041625ab7950b63b1f09e4a6
Antti Karvinen <anttikarvinen@hotmail.com>
2021-06-08 18:16:19 +0300

    - DialogScoreAmount finished.

--- app/src/main/java/fi/efelantti/frisbeegolfer/fragment/DialogScoreAmount.kt
+++ app/src/main/java/fi/efelantti/frisbeegolfer/fragment/DialogScoreAmount.kt
@@ -4,12 +4,12 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import fi.efelantti.frisbeegolfer.R
 import fi.efelantti.frisbeegolfer.databinding.DialogScoreAmountBinding
 
-class DialogScoreAmount(private val onScoreAmountSelected: OnScoreAmountSelected) :
+class DialogScoreAmount(private val onScoreAmountSelectedListener: OnScoreAmountSelected) :
     DialogFragment() {
 
     private var _binding: DialogScoreAmountBinding? = null
@@ -24,16 +24,42 @@ class DialogScoreAmount(private val onScoreAmountSelected: OnScoreAmountSelected
         val dialog = AlertDialog.Builder(requireContext())
             .setView(binding.root)
             .setTitle(R.string.dialog_score_amount_title)
-            .setPositiveButton(getString(R.string.ok)) { _, _ ->
-                Toast.makeText(
-                    requireContext(),
-                    binding.scoreAmount.text.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+            .setPositiveButton(getString(R.string.set_score), null)
+            .setNegativeButton(getString(R.string.cancel), null)
             .create()
         return dialog
     }
+
+    override fun onResume() {
+        super.onResume()
+        val alertDialog = dialog as AlertDialog?
+        val okButton: Button = alertDialog!!.getButton(AlertDialog.BUTTON_POSITIVE)
+        okButton.setOnClickListener {
+            val maxNumberOfThrows = resources.getInteger(R.integer.max_amount_of_throws)
+            try {
+                val scoreAmount = parseScoreAmount(binding.scoreAmount.text.toString())
+                if (scoreAmount in 1..maxNumberOfThrows) {
+                    onScoreAmountSelectedListener.selectedScoreAmount(scoreAmount)
+                    dismiss()
+                } else {
+                    throw IllegalArgumentException("Score amount should be between 1 and $maxNumberOfThrows.")
+                }
+            } catch (exception: Exception) {
+                binding.scoreAmountLayout.error =
+                    getString(R.string.dialog_score_amount_error, maxNumberOfThrows)
+            }
+        }
+    }
+
+    private fun performOkButtonAction() {
+        // Do your stuff here
+    }
+
+    private fun parseScoreAmount(scoreAmountString: String): Int {
+        if (scoreAmountString.isBlank()) throw IllegalArgumentException("Can't get integer from empty string.")
+        return scoreAmountString.toInt()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
